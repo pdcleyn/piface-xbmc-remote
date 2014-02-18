37809d4d8734626762a572c4160d11f1c78e3f79
Peter De Cleyn <peter@decleyn.net>
2014-02-18 19:57:15 +0100

    Added command line parameters and debug output

--- piface-xbmc-remote.py
+++ piface-xbmc-remote.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+
+import sys
+import argparse
 import threading
 from time import sleep
 import pifacedigitalio
@@ -8,9 +12,12 @@ import json
 
 
 class remote(object):
-    def __init__(self):
-        self.url = "http://10.0.1.100:8081/jsonrpc?request="
+    def __init__(self,host,port,username,password,debug):
+        self.debug = debug
+        self.url = "http://"+host+":"+port+"/jsonrpc?request="
         self.headers = {'content-type': 'application/json'}
+        self.username = username
+        self.password = password
         self.keys = { 0 : { "jsonrpc": "2.0", "method": "INPUT.up", "id":"piface" },
                         1 : { "jsonrpc": "2.0", "method": "INPUT.down", "id":"piface" },
                         2 : { "jsonrpc": "2.0", "method": "INPUT.left", "id":"piface" },
@@ -18,14 +25,16 @@ class remote(object):
         }
 
     def press(self,buttonID):
-        print(self.keys[buttonID])
-        r = requests.post(self.url, data=json.dumps(self.keys[buttonID]),headers=self.headers,auth=HTTPBasicAuth('xbmc', 'xbmc'))
+        if self.debug == True:
+            print(self.keys[buttonID])
+        r = requests.post(self.url, data=json.dumps(self.keys[buttonID]),headers=self.headers,auth=HTTPBasicAuth(self.username, self.password))
 
     
 
 class face(object):
-    def __init__(self,remote):
+    def __init__(self,remote,debug):
         self.remote = remote
+        self.debug = debug
         self.should_stop = False
         self.pifacedigital = pifacedigitalio.PiFaceDigital()
         self.inputlistener = pifacedigitalio.InputEventListener(chip=self.pifacedigital)
@@ -34,29 +43,38 @@ class face(object):
         self.inputlistener.activate()
 
     def button_pressed(self, event):
-        print("Button pressed", event.pin_num) 
+        if self.debug:
+            print("Button pressed", event.pin_num) 
         self.remote.press(event.pin_num)
         
     def start(self):
         while not self.should_stop:
-            print(".")
             sleep(1)
            
         self.inputlistener.deactivate()
         
     def stop(self, new_value):
         self.should_stop = new_value
+
+
+def main(argv):
+    usage =  'piface-xbmc-remote.py -x <xbmc hostname or IP address>'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", help = "hostname or IP address of the XBMC instance you want to control")
+    parser.add_argument("port", help = "port where the XBMC webservice is reachable")
+    parser.add_argument("username", help = "username to control the XBMC webinterface")
+    parser.add_argument("password", help = "password for the XBMC webinterface")
+    parser.add_argument("--debug", help="enable debug output",action="store_true")
+    args = parser.parse_args()
+
+
+    print("initializing ...")
+    r = remote(args.host,args.port,args.username,args.password,args.debug)
+    buttons = face(r,args.debug)        
+
+    buttons.start()    
        
 if __name__ == "__main__":
-    print("initializing ...")
-    pygame.init()
-    r = remote()
-    buttons = face(r)        
-    buttons.start()
-    print("started")
+    main(sys.argv[1:])
     
-    for event in pygame.event.get():
-        if event.type==QUIT:
-            print("Stopping")
-            buttons.stop(True)
-            pygame.quit()
